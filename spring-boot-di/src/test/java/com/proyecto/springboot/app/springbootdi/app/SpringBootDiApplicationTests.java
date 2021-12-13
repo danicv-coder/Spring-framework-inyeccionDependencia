f992ecdc2a10e6db6902209b334768ec4ddcd34f
Daniel Calderon <daniel.calderon.e.v@gmail.com>
2021-12-13 14:51:44 -0300

    .

---
+++ spring-boot-di/src/test/java/com/proyecto/springboot/app/springbootdi/app/SpringBootDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.springboot.app.springbootdi.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
